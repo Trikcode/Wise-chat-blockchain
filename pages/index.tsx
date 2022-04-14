e65cc549c4b4dfcf7d0557e4eb3b67ab783e67c8
Nobert Patrick <ayesiganobert@gmail.com>
2022-04-15 02:03:52 +0300

    set home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,71 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import { useWallet } from '@solana/wallet-adapter-react'
+import SignUp from '../components/SignUp'
+import Header from '../components/Header'
+import Feed from '../components/Feed'
+import RightSidebar from '../components/RightSidebar'
+import Sidebar from '../components/Sidebar'
 
-const Home: NextPage = () => {
+const style = {
+  wrapper: `bg-[#18191a] min-h-screen duration-[0.5s]`,
+  homeWrapper: `flex`,
+  center: `flex-1`,
+  main: `flex-1 flex justify-center`,
+  signupContainer: `flex items-center justify-center w-screen h-[70vh]`,
+}
+
+export default function Home() {
+  const [registered, setRegistered] = useState(false)
+  const [name, setName] = useState('')
+  const [url, setUrl] = useState('')
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      await requestUsersData()
+    })()
+  }, [])
+
+  const wallet = useWallet()
+
+  const requestUsersData = async activeAccount => {
+    try {
+      const response = await fetch(`/api/fetchUsers`)
+      const data = await response.json()
+
+      setUsers(data.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className={style.wrapper}>
+      <Header name={name} url={url} />
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      {registered ? (
+        <div className={style.homeWrapper}>
+          <Sidebar name={name} url={url} />
+          <div className={style.main}>
+            <Feed connected={wallet.connected} name={name} url={url} />
+          </div>
+          <RightSidebar
+            getUsers={requestUsersData}
+            users={users}
+            setUsers={setUsers}
+          />
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      ) : (
+        <div className={style.signupContainer}>
+          <SignUp
+            setRegistered={setRegistered}
+            name={name}
+            setName={setName}
+            url={url}
+            setUrl={setUrl}
+          />
+        </div>
+      )}
     </div>
   )
 }
-
-export default Home
